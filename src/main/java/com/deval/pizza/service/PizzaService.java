1e4a8fa2186a63e6bb58b4d5f902060a26e4c969
4lejandroquintero <alejoq96@gmail.com>
2023-11-07 21:50:03 -0500

    Inicio de Piizaservice

---
+++ src/main/java/com/deval/pizza/service/PizzaService.java
@@ -0,0 +1,15 @@
+package com.deval.pizza.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PizzaService {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PizzaService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
