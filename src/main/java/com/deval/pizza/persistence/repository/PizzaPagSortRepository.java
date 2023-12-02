b21d61436dfb8c943afd484d607124dbc4dc7015
4lejandroquintero <alejoq96@gmail.com>
2023-12-02 18:19:18 -0500

    Paginacion en las consultas

---
+++ src/main/java/com/deval/pizza/persistence/repository/PizzaPagSortRepository.java
@@ -0,0 +1,7 @@
+package com.deval.pizza.persistence.repository;
+
+import com.deval.pizza.persistence.entity.PizzaEntity;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
+
+public interface PizzaPagSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+}
