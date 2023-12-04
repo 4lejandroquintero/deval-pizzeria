37b9f1e2811a06dfa7469f21dc8189bf6beec724
4lejandroquintero <alejoq96@gmail.com>
2023-12-04 18:19:11 -0500

    Deshabilitar proteccion CSRF

--- src/main/java/com/deval/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/deval/pizza/web/config/SecurityConfig.java
@@ -11,6 +11,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated()
